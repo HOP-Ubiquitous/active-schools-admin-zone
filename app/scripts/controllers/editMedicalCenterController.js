8728612685a7d125c0a850810e62a99a262d05e8
scanovas <s.canovas@libelium.com>
2023-02-01 10:52:03 +0100

    Medical Centers

---
+++ app/scripts/controllers/editMedicalCenterController.js
@@ -0,0 +1,97 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name activeSchoolsAdminZoneApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the activeSchoolsAdminZoneApp
+ */
+
+app.controller('editMedicalCtrl', ['$scope', '$location', 'medicalCenterService', 'medicalCenterServiceData', '$routeParams', 'ICONS', 'COUNTRIES',
+  function ($scope, $location, medicalCenterService, medicalCenterServiceData, $routeParams, ICONS, COUNTRIES) {
+
+    var vm = this;
+    vm.icons = ICONS;
+    vm.countries = COUNTRIES.countries;
+    vm.categories = [
+      {
+        name: 'Aerobics',
+        value: 'aerobics'
+      },
+      {
+        name: 'Balance',
+        value: 'balance'
+      },
+      {
+        name: 'Mental',
+        value: 'mental'
+      },
+      {
+        name: 'Strength',
+        value: 'strength'
+      },
+      {
+        name: 'Stretch',
+        value: 'stretch'
+      }
+    ];
+    vm.units = [
+      {
+        name: 'Minutes',
+        value: 'minutes'
+      },
+      {
+        name: 'Seconds',
+        value: 'seconds'
+      },
+      {
+        name: 'Repeats',
+        value: 'reps'
+      }
+    ];
+    vm.id = $routeParams.medical_id;
+
+    medicalCenterService.getMedicalCenterById(vm.id);
+
+    function getMedicalCenter () {
+      vm.medical_center = medicalCenterServiceData.medicalCenterById;
+    }
+
+    vm.getUnit = function (unit) {
+      vm.challenge.unit = unit;
+    };
+
+    vm.edit = function () {
+
+      let medical_center =  {
+        medical_center_name: vm.medical_center.medical_center_name,
+        medical_center_address: vm.medical_center.medical_center_address,
+        medical_center_postal_code: vm.medical_center.medical_center_postal_code,
+        medical_center_city: vm.medical_center.medical_center_city,
+        medical_center_province: vm.medical_center.medical_center_province,
+        medical_center_country: vm.medical_center.medical_center_country
+      };
+
+      medicalCenterService.editMedicalCenter(vm.id, medical_center);
+
+  };
+
+    function initWatchers() {
+
+      vm.postWatcher = $scope.$watch(
+        function () {
+          return medicalCenterService.medicalCenterByIdLoaded;
+        }, function (newValue) {
+          if (newValue === true) {
+            getMedicalCenter();
+            medicalCenterService.medicalCenterByIdLoaded = false;
+          }
+        }
+      );
+
+    }
+
+    initWatchers();
+
+  }]);
